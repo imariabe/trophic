cf2d79f15212ccf8d1b9fb474ecea9477c1fe407
imariabe <143972990+imariabe@users.noreply.github.com>
2024-10-11 11:05:10 +0100

    update website files

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function toggleMenu() {
     const header = document.querySelector('.header2');
     header.classList.toggle('active');
 }
+
+function animateCounter() {
+    const counters = document.querySelectorAll('.count');
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-count');
+        const count = +counter.innerText;
+  
+        const speed = 200;
+        const increment = target / speed;
+  
+        if (count < target) {
+          counter.innerText = Math.ceil(count + increment);
+          setTimeout(updateCount, 10);
+        } else {
+          counter.innerText = target;
+        }
+      };
+  
+      updateCount();
+    });
+  }
+  
+  window.addEventListener('load', animateCounter);
+  
