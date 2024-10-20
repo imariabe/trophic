cc5f46d6f71ce549649ea8d58ada35bbf86dc4c5
imariabe <143972990+imariabe@users.noreply.github.com>
2024-10-20 14:14:39 +0100

    update website files

--- script.js
+++ script.js
@@ -3,27 +3,44 @@ function toggleMenu() {
     header.classList.toggle('active');
 }
 
-function animateCounter() {
-    const counters = document.querySelectorAll('.count');
-    counters.forEach((counter) => {
-      const updateCount = () => {
-        const target = +counter.getAttribute('data-count');
-        const count = +counter.innerText;
-  
-        const speed = 200;
-        const increment = target / speed;
-  
-        if (count < target) {
-          counter.innerText = Math.ceil(count + increment);
-          setTimeout(updateCount, 10);
-        } else {
-          counter.innerText = target;
-        }
-      };
-  
-      updateCount();
-    });
-  }
-  
-  window.addEventListener('load', animateCounter);
-  
+function animateCounter(counter) {
+  const target = +counter.getAttribute('data-count');
+  const speed = 500; // Adjust speed for slower count
+  let count = 0;
+
+  const updateCount = () => {
+    const increment = target / speed;
+    count += increment;
+    if (count < target) {
+      counter.innerText = Math.ceil(count);
+      setTimeout(updateCount, 30); // Increase delay for slower count
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+}
+
+function startCounters() {
+  const counters = document.querySelectorAll('.count');
+  counters.forEach(counter => animateCounter(counter));
+}
+
+const observerOptions = {
+  root: null, // Sets it to viewport
+  threshold: 0.2 // Trigger when 20% of the section is visible
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      startCounters();
+      observer.unobserve(entry.target); // Stop observing once animation starts
+    }
+  });
+}, observerOptions);
+
+document.querySelectorAll('.stat-item').forEach(statItem => {
+  observer.observe(statItem);
+});
